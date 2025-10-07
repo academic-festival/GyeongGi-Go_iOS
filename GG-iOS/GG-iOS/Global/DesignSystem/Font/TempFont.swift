13fc0df88fbc91a1d59a15354b1df6f5691e6e13
Won <andrew112517@naver.com>
2025-10-07 23:55:41 +0900

    [chore/#3] delete TempFiles

--- GG-iOS/GG-iOS/Global/DesignSystem/Font/TempFont.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TempFont.swift
-//  GG-iOS
-//
-//  Created by 김승원 on 10/1/25.
-//
-
-import Foundation
--- GG-iOS/GG-iOS/Global/Util/TempUtil.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TempUtil.swift
-//  GG-iOS
-//
-//  Created by 김승원 on 10/1/25.
-//
-
-import Foundation
