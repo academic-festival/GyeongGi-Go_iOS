1ccf0ec1ae42c84e5a41096aebcf76be820217c9
Won <andrew112517@naver.com>
2025-11-04 21:58:22 +0900

    [fix/#16] fix curator header ui

--- GG-iOS/GG-iOS/Global/Component/PlaceCuratorHeader.swift
+++ GG-iOS/GG-iOS/Global/Component/PlaceCuratorHeader.swift
@@ -11,41 +11,69 @@ struct PlaceCuratorHeader: View {
     
     // MARK: - Properties
     
+    private let sheetState: SheetState
     private var onTap: (() -> Void)?
     
     // MARK: - Initializer
     
-    init(onTap: (() -> Void)? = nil) {
+    init(
+        _ sheetState: SheetState,
+        onTap: (() -> Void)? = nil
+    ) {
+        self.sheetState = sheetState
         self.onTap = onTap
     }
     
     // MARK: - Body
     
     var body: some View {
-        HStack(alignment: .center, spacing: 10.adjustedWidth) {
+        HStack(alignment: .center, spacing: 0) {
             Image(.logoMainSmall)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 32.adjustedWidth, height: 21.adjustedHeight)
             
-            Button {
-                onTap?()
-            } label: {
-                Text("Discover your curated story")
-                    .applyGGFont(.body01)
-                    .foregroundStyle(.mainOrange500)
-                    .padding(.horizontal, 12.adjustedWidth)
-                    .padding(.vertical, 8.adjustedHeight)
-                    .background(.gray0)
-                    .cornerRadius(10, corners: .allCorners)
-                    .addBorder(.roundedRectangle(cornerRadius: 10), borderColor: .gray200, borderWidth: 1)
+            switch sheetState {
+            case .list:
+                curatorHeader
+            case .detail:
+                curatorButton
             }
-            .buttonStyle(.plain)
         }
     }
 }
 
+// MARK: - Subviews
+
+extension PlaceCuratorHeader {
+    private var curatorHeader: some View {
+        Text("Discover your curated story")
+            .applyGGFont(.body01)
+            .foregroundStyle(.mainOrange500)
+            .padding(.horizontal, 12.adjustedWidth)
+            .padding(.vertical, 8.adjustedHeight)
+    }
+    
+    private var curatorButton: some View {
+        Button {
+            onTap?()
+        } label: {
+            Text("Start your curated story")
+                .applyGGFont(.body01)
+                .foregroundStyle(.mainOrange500)
+                .padding(.horizontal, 12.adjustedWidth)
+                .padding(.vertical, 8.adjustedHeight)
+                .background(.gray0)
+                .cornerRadius(10, corners: .allCorners)
+                .addBorder(.roundedRectangle(cornerRadius: 10), borderColor: .gray200, borderWidth: 1)
+        }
+        .buttonStyle(.plain)
+        .padding(.leading, 10.adjustedWidth)
+    }
+}
+
 #Preview {
-    PlaceCuratorHeader()
+    PlaceCuratorHeader(.list)
+    PlaceCuratorHeader(.detail)
 }
