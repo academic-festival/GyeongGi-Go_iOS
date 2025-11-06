15c2fb57f7946fdcc343eb505155131b13adbe65
Won <andrew112517@naver.com>
2025-11-06 19:30:26 +0900

    [chore/#18] change placeList, placeDetail file location

--- GG-iOS/GG-iOS/Presentation/PlaceDetail/ViewModel/PlaceDetailViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PlaceDetailViewModel.swift
-//  GG-iOS
-//
-//  Created by 김승원 on 10/9/25.
-//
-
-import Foundation
--- GG-iOS/GG-iOS/Presentation/PlaceList/ViewModel/PlaceListViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PlaceListViewModel.swift
-//  GG-iOS
-//
-//  Created by 김승원 on 10/9/25.
-//
-
-import Foundation
