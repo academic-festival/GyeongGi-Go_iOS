8581276fe74d98813c3c3ddd1d138e9b2e8b95d2
Won <andrew112517@naver.com>
2025-11-12 19:49:30 +0900

    [feat/#20] create BaseTargetType

---
+++ GG-iOS/GG-iOS/Network/Base/BaseTargetType.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTargetType.swift
+//  GG-iOS
+//
+//  Created by 김승원 on 11/12/25.
+//
+
+import Foundation
+
+import Moya
+
+protocol BaseTargetType: TargetType {
+
+}
+
+extension BaseTargetType {
+    var baseURL: URL {
+        guard let url = URL(string: "\(AppEnvironment.baseURL)/api") else {
+            fatalError("Error: BaseURL을 찾을 수 없습니다.")
+        }
+        
+        return url
+    }
+    
+    var headers: [String: String]? {
+        var header = ["Content-Type": "application/json"]
+        return header
+    }
+}
