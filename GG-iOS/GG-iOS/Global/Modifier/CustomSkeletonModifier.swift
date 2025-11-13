8b267cc404405a140bd9f5c771fa92b1cd7c2147
Won <andrew112517@naver.com>
2025-11-13 17:30:24 +0900

    [feat/#28] create CustomSkeletonModifier

---
+++ GG-iOS/GG-iOS/Global/Modifier/CustomSkeletonModifier.swift
@@ -0,0 +1,45 @@
+//
+//  CustomSkeletonModifier.swift
+//  GG-iOS
+//
+//  Created by 김승원 on 11/13/25.
+//
+
+import SwiftUI
+
+struct CustomSkeletonModifier: ViewModifier {
+    private let isLoading: Bool
+    private let size: CGSize?
+    private let radius: CGFloat
+    
+    init(isLoading: Bool, size: CGSize?, radius: CGFloat) {
+        self.isLoading = isLoading
+        self.size = size
+        self.radius = radius
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .skeleton(
+                with: isLoading,
+                size: size,
+                shape: .rounded(.radius(radius))
+            )
+    }
+}
+
+extension View {
+    func customSkeleton(
+        with isLoading: Bool,
+        size: CGSize? = nil,
+        radius: CGFloat = 10
+    ) -> some View {
+        self.modifier(
+            CustomSkeletonModifier(
+                isLoading: isLoading,
+                size: size,
+                radius: radius
+            )
+        )
+    }
+}
