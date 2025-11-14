b154875cac9b7a3863634292473b3cdc39fe49dc
Won <andrew112517@naver.com>
2025-11-14 17:44:48 +0900

    [feat/#37] create audio bubble UI

--- GG-iOS/GG-iOS/Presentation/Chat/Model/ChatMessage.swift
+++ GG-iOS/GG-iOS/Presentation/Chat/Model/ChatMessage.swift
@@ -5,12 +5,13 @@
 //  Created by 김승원 on 11/10/25.
 //
 
-import Foundation
+import SwiftUI
 
 struct ChatMessage: Identifiable {
     let id = UUID()
     let sender: Sender
     let message: String
+    let audioData: Data?
 }
 
 enum Sender {
@@ -18,13 +19,31 @@ enum Sender {
     case user
 }
 
+enum AudioPlayState {
+    case playing
+    case paused
+    
+    var icon: ImageResource {
+        switch self {
+        case .playing:
+            return .pauseIcon
+        case .paused:
+            return .playIcon
+        }
+    }
+}
+
 
 extension ChatMessage {
     static var mockData: [ChatMessage] {
         [
-            ChatMessage(sender: .chatBot, message: "From now on, I will give you a brief introduction and information about your surroundings. "),
-            ChatMessage(sender: .user, message: "I am curious about Suwon Hwaseong."),
-            ChatMessage(sender: .chatBot, message: "Hwaseong Fortress in Suwon is a UNESCO World Heritage site built during the late 18th century by King Jeongjo of the Joseon Dynasty. It was constructed to honor his father and to create a strong, strategic city. The fortress features impressive walls, gates, and military facilities that showcase both Korean and early modern engineering techniques. Today, it is a cultural landmark where visitors can walk along the walls and enjoy panoramic views of the city.")
+            ChatMessage(sender: .chatBot, message: "From now on, I will give you a brief introduction and information about your surroundings. ", audioData: nil),
+            ChatMessage(sender: .user, message: "I am curious about Suwon Hwaseong.", audioData: nil),
+            ChatMessage(
+                sender: .chatBot,
+                message: "Hwaseong Fortress in Suwon is a UNESCO World Heritage site built during the late 18th century by King Jeongjo of the Joseon Dynasty. It was constructed to honor his father and to create a strong, strategic city. The fortress features impressive walls, gates, and military facilities that showcase both Korean and early modern engineering techniques. Today, it is a cultural landmark where visitors can walk along the walls and enjoy panoramic views of the city.",
+                audioData: Data()
+            )
         ]
     }
 }
